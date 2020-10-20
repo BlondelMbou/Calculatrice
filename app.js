562f16efe2fc83e34ea47e6564af1f62d3890b08
BlondelMbou <73158427+BlondelMbou@users.noreply.github.com>
2020-10-20 09:38:42 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+//DOM(Document Object Model)
+const touches = document.querySelectorAll('.bouton');
+Console.log(touches);
+//const listekeycode = touches.map(touche => touche.dataset.key);
+//Console.log(listekeycode);
